0c0dc871a296af502ff8ae6a46eedbb2c8ab0270
fiamaBrenda <fiamabrenda94@gmail.com>
2023-08-11 21:53:25 -0300

    Adicionando props, hooks e state

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './index.css'
 import ListaServicos from './pages/ListaServicos'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import ListaDevs from './pages/ListaDevs'
 
 // rotas
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='lista/servicos' element={<ListaServicos/>} />
+        <Route path='ListaDevs' element={<ListaDevs/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
